b27b4117c103946764561779baf65d838e281607
David <tdshuma@outlook.com>
2022-06-27 09:29:09 +0200

    update images with dynamic

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import Splide from '@splidejs/splide';
 
 @Component({
@@ -7,7 +7,18 @@ import Splide from '@splidejs/splide';
   styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent implements AfterViewInit {
-  constructor() {}
+  public imageLinks: Array<String>;
+
+  constructor() {
+    this.imageLinks = [
+      '26acc584174fb0981864c900047d33db',
+      'f70985aa9945b63756a6eeef2a66b19d',
+      '47943008ca49feabdca3c9391c3b8f8a',
+      'adf9db21a43c9fd394dc89a0d647bec3',
+      'a9d3e037a470bfe3c367dc803480a848',
+      '66408741d426b4271f9f59df31a3bab4',
+    ];
+  }
 
   ngAfterViewInit(): void {
     new Splide('.splide', {
